4860cce8a92d468d6690564a670b6540cdaf5c48
Kabbo <157718635+kabbo777@users.noreply.github.com>
2026-09-09 14:18:45 +0600

    Update Config.php

--- config/Config.php
+++ config/Config.php
@@ -2,11 +2,12 @@
 class Config {
     public static function get($key, $default = null) {
         $config = [
-            'db_host' => getenv('DB_HOST') ?: '127.0.0.1',
-            'db_name' => getenv('DB_NAME') ?: 'blood_network',
-            'db_user' => getenv('DB_USER') ?: 'root',
-            'db_pass' => getenv('DB_PASS') ?: '',
-            'gemini_api_key' => getenv('GEMINI_API_KEY') ?: ''
+            'db_host'             => getenv('DB_HOST') ?: '127.0.0.1',
+            'db_name'             => getenv('DB_NAME') ?: 'smart_blood_db',
+            'db_user'             => getenv('DB_USER') ?: 'root',
+            'db_pass'             => getenv('DB_PASS') ?: '',
+            'gemini_api_key'      => getenv('GEMINI_API_KEY') ?: 'AQ.Ab8RN6KSavrQfq3ejFyMeSuvXhAs1gSf2Ftm8EhiwfIlrd_CMw',
+            'google_maps_api_key' => getenv('GOOGLE_MAPS_API_KEY') ?: 'AIzaSyAPrT7Xra9IdrxJKqyoOJ5IcNm0urEOE28'
         ];
 
         return $config[$key] ?? $default;
